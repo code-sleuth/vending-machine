05d650af6447a6ad4ed3d668374d0df79f9db284
Ibrahim Mbaziira <code.ibra@gmail.com>
2021-06-30 03:21:16 +0300

    cleanup

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -5,5 +5,4 @@ import "github.com/gorilla/mux"
 // StartUp function registers all routes
 func StartUp(mux *mux.Router) {
 	UserController{mux}.registerRoutes()
-	TransactionController{mux}.registerRoutes()
 }
